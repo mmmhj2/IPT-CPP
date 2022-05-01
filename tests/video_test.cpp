8cfc1ce87a36e6a0b97895e7438419288f72d3b4
Lee <li20020410@126.com>
2022-05-01 15:51:03 +0800

    added a new unit test for video capture
    Signed-off-by: Lee <li20020410@126.com>

---
+++ tests/video_test.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+
+using std::cout;
+using std::endl;
+constexpr char DEVICENAME[] = "/dev/video0";
+
+int main(int argc, char * argv[])
+{
+	cv::VideoCapture vCapture{ DEVICENAME };
+
+	if (!vCapture.isOpened())
+	{
+		cout << "Cannot open device \"" << DEVICENAME << "\"" << endl;
+		return -1;
+	}
+
+	int height, width, fps, channels;
+	height = vCapture.get(cv::CAP_PROP_FRAME_HEIGHT);
+	width = vCapture.get(cv::CAP_PROP_FRAME_WIDTH);
+	fps = vCapture.get(cv::CAP_PROP_FPS);
+	channels = vCapture.get(cv::CAP_PROP_CHANNEL);
+	
+	cout << "Video properties : "
+		<< height << "x" << width << " with "
+		<< fps << "FPS (" << channels << " channels)" << endl;
+
+	cout << "Setting properites to desired value ..." << endl;
+
+	bool result;
+	result = vCapture.set(cv::CAP_PROP_FRAME_HEIGHT, 360);
+	result &= vCapture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+	result &= vCapture.set(cv::CAP_PROP_FPS, 120);
+
+	if (!result)
+		cout << "Failed to set properities" << endl;
+	
+	cv::Mat image(360, 640, CV_8UC3);
+	vCapture >> image;
+	cv::imwrite("image.png", image);
+
+	return 0;
+}
